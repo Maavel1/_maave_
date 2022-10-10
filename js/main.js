11c245b4aa5e4a927dc71ea1f0f289be15b3c093
Maks-ber <98887361+Maks-ber@users.noreply.github.com>
2022-10-11 00:39:12 +0600

    2222

--- js/main.js
+++
@@ -1,8 +0,0 @@
-let menuBtn = document.querySelector('.menu_btn');
-let menu = document.querySelector('.menu');
-let body = document.querySelector('body')
-menuBtn.addEventListener('click', function(){
-	menuBtn.classList.toggle('active');
-	menu.classList.toggle('active');
-    body.classList.toggle('active')
-});
